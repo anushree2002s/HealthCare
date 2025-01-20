31d662a4bea67a21de36a6942eff937a00d0b0af
Anushree <anushree2002s@gmail.com>
2025-01-20 21:42:20 +0545

    carousel done

---
+++ script.js
@@ -0,0 +1,20 @@
+ // rotating carousel of words
+ const illnesses = [
+    "COVID-19 Symptoms",
+    "Flu Symptoms",
+    "Chronic Pain",
+    "Sports Injuries",
+    "Headaches",
+    "Respiratory Issues",
+    "That Weird Looking Rash"
+  ];
+  let currentIndex = 0;
+
+  function rotateIllness() {
+    const illnessElement = document.getElementById("illness-carousel");
+    currentIndex = (currentIndex + 1) % illnesses.length;
+    illnessElement.textContent = illnesses[currentIndex];
+  }
+
+//change every 3 secs
+  setInterval(rotateIllness, 3000);
